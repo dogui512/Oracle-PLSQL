40fafc96cf355fa072e7324f815f84f733d50ff3
Guido Ramírez <49038278+dogui512@users.noreply.github.com>
2019-04-27 16:49:27 -0300

    Update puntoDecimalPorComa.sql

--- puntoDecimalPorComa.sql
+++ puntoDecimalPorComa.sql
@@ -1,6 +1,7 @@
 /*
 /* Guido Ramírez
 /* Link de referencia: https://docs.oracle.com/cd/B19306_01/server.102/b14200/functions191.htm
+/* La siguiente sentencia permite cambiar el punto (.) decimal por coma (,).
 */
 
 SELECT TO_CHAR(-10000,'L99G999D99MI',
